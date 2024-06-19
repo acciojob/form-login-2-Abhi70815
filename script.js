2dcdfdb4def26e26c2625fb39bfd38add7cc09e5
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-19 15:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ function displayAlert() {
+            let fname = document.getElementById('first-name').value;
+            let lname=document.getElementById('last-name').value;
+            let num=document.getElementById('phone-number').value;
+            let email= document.getElementById('email-id').value;
+
+            alert(" First Name: " + fname + " Last Name: " + lname + " Phone Number: " + num + " Email ID: " + email);
+
+        }
